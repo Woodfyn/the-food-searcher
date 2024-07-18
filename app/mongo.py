1785ffa1406fd1bb6e29c939a01fa6bca72ac85a
Artem <artemrudyi2006@gmail.com>
2024-07-18 16:23:15 +0300

    fix

--- app/mongo.py
+++ app/mongo.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo import MongoClient
 
+
 class Mongo:
     def __init__(self, client: MongoClient):
         database = client['the_food_searcher']
@@ -9,15 +10,33 @@ class Mongo:
         self.user_collection = database['users']
 
     async def create_user(self, user_id: int, height: float, weight: float, age: float, sex: str, factor: float, plan: int):
-        self.user_collection.insert_one({
-            '_id': user_id,
-            'height': height,
-            'weight': weight,
-            'age': age,
-            'sex': sex,
-            'factor': factor,
-            'plan': plan
-        })
+        if not self.user_collection.find_one({'_id': user_id}):
+            self.user_collection.insert_one({
+                '_id': user_id,
+                'height': height,
+                'weight': weight,
+                'age': age,
+                'sex': sex,
+                'factor': factor,
+                'plan': plan
+            })
+        else:
+            print('User already exists')
+
+    async def create_or_update_user(self, user_id: int, height: float, weight: float, age: float, sex: str, factor: float, plan: int):
+        self.user_collection.replace_one(
+            {'_id': user_id},
+            {
+                '_id': user_id,
+                'height': height,
+                'weight': weight,
+                'age': age,
+                'sex': sex,
+                'factor': factor,
+                'plan': plan
+            },
+            upsert=True
+        )
 
     async def get_user_by_id(self, user_id: int):
         return self.user_collection.find_one({'_id': user_id})
